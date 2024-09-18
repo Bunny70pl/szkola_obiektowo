34df40b7f68dae0f9f3c1e2f33e0f7322c3ec8a0
Bunny70pl <bunny70pl@gmail.com>
2024-09-18 12:09:04 +0200

    CLASA GETTERY I SETTERY

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Osoba osobaJas = new Osoba("Jas","Rakowski",7);
+        System.out.println(osobaJas.getImie());
+        System.out.println(osobaJas.getNazwisko());
 
-        // nie zgodne z regula heremtyzacji
-        System.out.println(osobaJas.imie);
-        System.out.println(osobaJas.nazwisko);
-        System.out.println(osobaJas.wiek);
-
-        osobaJas.imie = "janek";
-        System.out.println(osobaJas.imie);
-        //hermetyzacja
-        //powinno byc tak zeby program glowny nie mogl zmieniac wlasnosci klas
+        osobaJas.setImie("Marus");
+        System.out.println(osobaJas.getImie());
     }
 }
--- src/Osoba.java
+++ src/Osoba.java
@@ -1,13 +1,51 @@
+import java.util.Scanner;
+
 public class Osoba {
-    String imie;
-    String nazwisko;
-    int wiek;
+    private String imie;
+    private String nazwisko;
+    private int wiek;
+    //prywatne wlasonosci sa dostepne tylko w TEJ KLASIE
 
     //konsturktor to metody wywloywana w moemncie tworzenia obiektu
 
     public Osoba(String imie, String nazwisko, int wiek) {
         this.imie = imie;
         this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
+
+
+    //Metody dosptepowe settery i gettery
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setImie(String imie) {
+        //wpisywanie hasla z klawiatury
+        Scanner klawa = new Scanner(System.in);
+        System.out.println("Podaj hasło: ");
+        String haslo = klawa.next();
+        //jezeli haslo qwe123 to zmien w przeciwnym razie odpowiedni komunikat
+        if(haslo.equals("qwe123")){
+            this.imie = imie;
+        }else{
+            System.out.println("Nie masz prawa zmieniac");
+        }
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public void setWiek(int wiek) {
         this.wiek = wiek;
     }
 }
