a3dd87dd890096b3b51faddd0e90bcc3a094511a
Bunny70pl <bunny70pl@gmail.com>
2024-09-18 13:24:19 +0200

    wychowawca

--- src/Nauczyciel.java
+++ src/Nauczyciel.java
@@ -1,6 +1,7 @@
-public class Nauczyciel extends Osoba{
+public sealed class Nauczyciel extends Osoba permits Wychowawca{
     private String przedmitot;
-
+    //seald ograczione dziedziceznie
+    //do czego moze
     public Nauczyciel(String imie, String nazwisko, int wiek, String przedmitot) {
         super(imie, nazwisko, wiek);
         this.przedmitot = przedmitot;
--- src/Wychowawca.java
+++ src/Wychowawca.java
@@ -1,3 +1,6 @@
-public class Wychowawca {
-
+public final class Wychowawca extends Nauczyciel {
+    //final koniec
+    public Wychowawca(String imie, String nazwisko, int wiek, String przedmitot) {
+        super(imie, nazwisko, wiek, przedmitot);
+    }
 }
