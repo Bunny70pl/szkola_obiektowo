00c64766fc11e900f30091ebe1b6a0ff3614fe81
Bunny70pl <bunny70pl@gmail.com>
2024-09-18 11:28:00 +0200

    brak modyfikatora dostepu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Osoba osobaJas = new Osoba("Jas","Rakowski",7);
+
+        // nie zgodne z regula heremtyzacji
+        System.out.println(osobaJas.imie);
+        System.out.println(osobaJas.nazwisko);
+        System.out.println(osobaJas.wiek);
+
+        osobaJas.imie = "janek";
+        System.out.println(osobaJas.imie);
+        //hermetyzacja
+        //powinno byc tak zeby program glowny nie mogl zmieniac wlasnosci klas
     }
 }
---
+++ src/Osoba.java
@@ -0,0 +1,13 @@
+public class Osoba {
+    String imie;
+    String nazwisko;
+    int wiek;
+
+    //konsturktor to metody wywloywana w moemncie tworzenia obiektu
+
+    public Osoba(String imie, String nazwisko, int wiek) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
+}
